06afe90dfaeb3c05052fc19f32d9ed730fce20b4
Mike Pennisi <mike@mikepennisi.com>
2014-12-03 00:18:51 -0500

    Try a CRAZY EXPERIMENT

--- src/client/modules/main.js
+++ src/client/modules/main.js
@@ -1,6 +1,18 @@
-'use strict';
+/*'use strict';
 var Landing = require('./components/landing/index');
 
 new Landing({
   el: document.body
+});
+*/
+require.config({
+  paths: {
+    node_modules: '/node_modules'
+  }
+});
+define(function(require) {
+  'use strict';
+  var State = require('node_modules/ampersand-state/ampersand-state');
+
+  console.log(State);
 });
--- src/server/index.js
+++ src/server/index.js
@@ -6,11 +6,13 @@ var browserifyMiddleware = require('browserify-middleware');
 
 var app = express();
 
-app.use('/modules/main.js', browserifyMiddleware(__dirname + '/../client/modules/main.js', {
+/*app.use('/modules/main.js', browserifyMiddleware(__dirname + '/../client/modules/main.js', {
   transform: [
     require('./ractive-transform')
   ]
-}));
+}));*/
+app.use('/node_modules/requirejs', express.static(__dirname + '/../../node_modules/requirejs'));
+app.use('/node_modules', browserifyMiddleware(__dirname + '/../../node_modules'));
 app.use(express.static(__dirname + '/../client'));
 
 var server = app.listen(8000, function() {
