498040e73eb15951656b7e4ce7863402a606861b
Mike Pennisi <mike@mikepennisi.com>
2015-01-07 18:26:43 -0500

    PARTIAL: Begin testing real user interaction
    This is so terribly ugly.

--- test/ui/driver.js
+++ test/ui/driver.js
@@ -2,6 +2,7 @@
 
 var selectors = require('./selectors.json');
 var lookup = require('./util/lookup');
+var dayNames = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
 
 function Driver(options) {
   this._cmd = options.command;
@@ -13,6 +14,22 @@ module.exports = Driver;
 Driver.prototype._$ = function(region) {
   var selector = lookup(region, selectors);
   return this._cmd.findAllByCssSelector(selector);
+};
+
+Driver.prototype._selectOption = function(element, value) {
+  var optionEls;
+  return element.findAllByTagName('option')
+    .then(function(options) {
+      optionEls = options;
+      return require('bluebird').all(options.map(function(o) { return o.getVisibleText(); }));
+    })
+    .then(function(text) {
+      var index = text.indexOf(value);
+
+      return this._cmd.execute(function(el) {
+        el.selected = true;
+      }, [optionEls[index]]);
+    }.bind(this));
 };
 
 Driver.prototype.read = function(region) {
@@ -34,5 +51,42 @@ Driver.prototype.get = function(path) {
     }.bind(this), function() {
       // If the login button is not present, the current session is already
       // authenticated and the operation is complete.
+    });
+};
+
+Driver.prototype.viewWeek = function(phaseNumber, weekNumber) {
+  return this._$('index.phaseWeekLink')
+    .then(function(weekLinks) {
+      return weekLinks[phaseNumber * 5 + weekNumber].click();
+    });
+};
+
+Driver.prototype.editUtilization = function(employeeNumber, dayName) {
+  var dayNumber = dayNames.indexOf(dayName);
+  var offset;
+
+  if (!dayNumber) {
+    throw new Error('Unrecognized day: "' + dayName + '".');
+  }
+
+  offset = employeeNumber * 5 + dayNumber;
+
+  return this._$('phaseWeek.day')
+    .then(function(days) {
+      return days[offset].click();
+    })
+    .then(function() {
+      return this._$('phaseWeek.typeInput');
+    }.bind(this)).then(function(typeInputs) {
+      return this._selectOption(typeInputs[offset], 'Sick Time');
+    }.bind(this))
+    .then(function() {
+      return this._$('phaseWeek.set');
+    }.bind(this))
+    .then(function(set) {
+      return set[offset].click();
+    })
+    .then(function() {
+      return require('bluebird').delay(2000);
     });
 };
