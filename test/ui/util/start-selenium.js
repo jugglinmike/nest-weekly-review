d0941556121463283ac3bd4d927acaf975f57248
Mike Pennisi <mike@mikepennisi.com>
2015-01-09 16:08:24 -0500

    Use safer mechanism for starting HTTP servers

--- test/ui/util/start-selenium.js
+++ test/ui/util/start-selenium.js
@@ -1,24 +1,26 @@
 'use strict';
 
+var Promise = require('bluebird');
 var spawn = require('child_process').spawn;
 
-var pollHttp = require('./poll-http');
+var portGuard = require('./port-guard');
 var jarFile = require('selenium-binaries').seleniumserver;
 
 module.exports = function(port) {
-  var child = spawn('java', [
-    '-jar', jarFile,
-    '-port', port
-  ]);
-  var kill = child.kill.bind(child);
+  return portGuard(port, function() {
+      var child = spawn('java', [
+        '-jar', jarFile,
+        '-port', port
+      ]);
+      var kill = function() {
+        child.kill();
 
-  return pollHttp(port).then(function() {
+        return new Promise(function(resolve, reject) {
+          child.once('exit', resolve);
+          child.once('error', reject);
+        });
+      };
+
       return kill;
-    }, function(err) {
-      try {
-        kill();
-      } catch (err) {}
-
-      throw err;
     });
 };
